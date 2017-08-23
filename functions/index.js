6ae1bc30c66c67b54bff1f7150b492e2a0acf465
pelupotter <n.tejeira69@gmail.com>
2017-08-23 13:01:09 -0300

    Fix :-D

--- functions/index.js
+++ functions/index.js
@@ -35,26 +35,26 @@ exports.createClientAccount = functions.database
 exports.sendWeightUpdate = functions.database
         .ref('/userProfile/{userId}/weightTrack/{weightId}')
         .onWrite(event => {
-            const clientId = event.params.userId;
-            const weight = event.data.val().weight;
-            return admin.database().ref(`/userProfile/${clientId}/`).once('value', clientProfileSnapshot => {
-                const coachId = clientProfileSnapshot.val().coachId;
-                const clientName = clientProfileSnapshot.val().fullName;
-                const clientStartingWeight = clientProfileSnapshot.val().startingWeight;
-                return admin.database().ref(`/userProfile/${coachId}/`).once('value', profileSnapshot => {
-                // Notification details.
-                    const payload = {
-                        notification:{
-                            title: "${clientName} just shared a weight update",
-                            body: "${clientName} started at ${clientStartingWeight} and just updated to ${weight}",
-                            sound: "default",
-                            click_action: "FCM_PLUGIN_ACTIVITY"
-                        },
-                        data :{ clientId: clientId }
-                    };
-                    return admin.messaging().sendToDevice(profileSnapshot.val().token, payload);
-                }).catch(error => {
-                    console.log("Error on sendWeightUpdate:", error);
+        const clientId = event.params.userId;
+        const weight = event.data.val().weight;
+            return admin.database().ref(`/userProfile/${clientId}/`)
+                .once('value', clientProfileSnapshot => {
+                    const coachId = clientProfileSnapshot.val().coachId;
+                    const clientName = clientProfileSnapshot.val().fullName;
+                    const clientStartingWeight = clientProfileSnapshot.val().startingWeight;
+                    return admin.database().ref(`/userProfile/${coachId}/`).once('value',
+                        profileSnapshot => {
+                            // Notification details.
+                        const payload = {
+                            "notification":{
+                                "title": `${clientName} just shared a weight update`,
+                                "body": `${clientName} started at ${clientStartingWeight} and just updated to ${weight}`,
+                                "sound":"default",
+                                "click_action":"FCM_PLUGIN_ACTIVITY"
+                            },
+                            "data":{ "clientId": clientId }
+                        };
+                        return admin.messaging().sendToDevice(profileSnapshot.val().token, payload);
+                    });
                 });
             });
-        });
